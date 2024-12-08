f9e866f46fae3ab61a2daa9c96fc377fe097f7c9
ifaamrillah <amrillah.work@gmail.com>
2024-12-08 13:37:54 +0700

    feat: add FormProps type

---
+++ src/lib/types.ts
@@ -0,0 +1,12 @@
+import { FieldValues, Path, UseFormReturn } from "react-hook-form";
+
+export type FormProps<T extends FieldValues> = {
+  form: UseFormReturn<T>;
+  name: Path<T>;
+  label?: string;
+  required?: boolean;
+  placeholder?: string;
+  disabled?: boolean;
+  description?: string;
+  className?: string;
+};
