1248c9e8d995ae7373566b6787b646189c7752c2
ifaamrillah <amrillah.work@gmail.com>
2024-12-08 19:03:39 +0700

    feat: add shadcn sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import { ReactQueryProvider } from "@/providers/react-query-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 import "./globals.css";
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ReactQueryProvider>{children}</ReactQueryProvider>
+        <Toaster richColors position="top-center" />
       </body>
     </html>
   );
