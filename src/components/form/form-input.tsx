56ae422ac32715b2a48106ec2a1db06473826d29
ifaamrillah <amrillah.work@gmail.com>
2024-12-08 13:38:34 +0700

    feat: add form input component

---
+++ src/components/form/form-input.tsx
@@ -0,0 +1,47 @@
+import { FieldValues } from "react-hook-form";
+
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { FormProps } from "@/lib/types";
+
+export const FormInput = <T extends FieldValues>({
+  form,
+  name,
+  label,
+  type = "text",
+  required,
+  placeholder = "Enter here",
+  disabled,
+  description,
+}: FormProps<T> & {
+  type?: string;
+}) => {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          {label && <FormLabel required={required}>{label}</FormLabel>}
+          <FormControl>
+            <Input
+              type={type}
+              placeholder={placeholder}
+              disabled={disabled}
+              {...field}
+            />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
