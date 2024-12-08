f3c5995726dac6a55cb3bd0e321f5149a6bd742e
ifaamrillah <amrillah.work@gmail.com>
2024-12-08 21:23:53 +0700

    feat: add validateFields function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { NextRequest, NextResponse } from "next/server";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { ZodType } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function validateFields<T>(
+  req: NextRequest,
+  validator: ZodType<T>
+) {
+  const body = await req.json();
+
+  const validated = validator.safeParse(body);
+
+  if (!validated.success) {
+    return NextResponse.json(
+      {
+        message: validated.error.errors[0]?.message || "Field validation error",
+      },
+      { status: 400 }
+    );
+  }
+
+  return validated.data;
 }
