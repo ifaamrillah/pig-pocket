f94162d9786bcc2c4e2d3c0fa3da1c896f2e8371
ifaamrillah <amrillah.work@gmail.com>
2024-12-08 21:26:11 +0700

    feat: add login credentials provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,33 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { prisma } from "@/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+      },
+      authorize: async (credentials) => {
+        const { email } = credentials;
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: email as string,
+          },
+        });
+
+        return user;
+      },
+    }),
+  ],
 });
