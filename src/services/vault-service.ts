fb6c1c8bd21c08638b8b4f1c86403111e9c22a15
ifaamrillah <amrillah.work@gmail.com>
2024-12-11 23:25:23 +0700

    feat: add get all vault service

--- src/services/vault-service.ts
+++ src/services/vault-service.ts
@@ -1,5 +1,12 @@
-import { apiPost } from "@/lib/axiosClient";
+import { apiGet, apiPost } from "@/lib/axiosClient";
 import { TypeVaultValidator } from "@/lib/validator";
+
+export async function getAllVault(params?: Record<string, unknown>) {
+  return await apiGet({
+    url: "/vault",
+    params,
+  });
+}
 
 export async function createVault(data: TypeVaultValidator) {
   return await apiPost({
