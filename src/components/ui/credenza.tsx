6be7695369886575ec45f4c220bab8d776307c13
ifaamrillah <amrillah.work@gmail.com>
2024-12-11 13:42:43 +0700

    feat: add credenza component

---
+++ src/components/ui/credenza.tsx
@@ -0,0 +1,175 @@
+"use client";
+
+import * as React from "react";
+
+import { useMediaQuery } from "@/hooks/use-media-query";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { cn } from "@/lib/utils";
+
+interface BaseProps {
+  children: React.ReactNode;
+}
+
+interface RootCredenzaProps extends BaseProps {
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+}
+
+interface CredenzaProps extends BaseProps {
+  className?: string;
+  asChild?: true;
+}
+
+const Credenza = ({
+  children,
+  open,
+  onOpenChange,
+  ...props
+}: RootCredenzaProps) => {
+  const { isDesktop } = useMediaQuery();
+  const [isOpen, setIsOpen] = React.useState(open || false);
+
+  // Update internal state when open prop changes (for controlled component)
+  React.useEffect(() => {
+    if (open !== undefined) {
+      setIsOpen(open);
+    }
+  }, [open]);
+
+  // When size changes, update open state if required
+  const CredenzaComponent = isDesktop ? Dialog : Drawer;
+
+  return (
+    <CredenzaComponent
+      {...props}
+      open={isOpen}
+      onOpenChange={(openState) => {
+        setIsOpen(openState);
+        onOpenChange?.(openState); // Allow external control of the open state
+      }}
+    >
+      {children}
+    </CredenzaComponent>
+  );
+};
+
+const CredenzaTrigger = ({ className, children, ...props }: CredenzaProps) => {
+  const { isDesktop } = useMediaQuery();
+  const CredenzaTrigger = isDesktop ? DialogTrigger : DrawerTrigger;
+
+  return (
+    <CredenzaTrigger className={className} {...props}>
+      {children}
+    </CredenzaTrigger>
+  );
+};
+
+const CredenzaClose = ({ className, children, ...props }: CredenzaProps) => {
+  const { isDesktop } = useMediaQuery();
+  const CredenzaClose = isDesktop ? DialogClose : DrawerClose;
+
+  return (
+    <CredenzaClose className={className} {...props}>
+      {children}
+    </CredenzaClose>
+  );
+};
+
+const CredenzaContent = ({ className, children, ...props }: CredenzaProps) => {
+  const { isDesktop } = useMediaQuery();
+  const CredenzaContent = isDesktop ? DialogContent : DrawerContent;
+
+  return (
+    <CredenzaContent className={className} {...props}>
+      {children}
+    </CredenzaContent>
+  );
+};
+
+const CredenzaDescription = ({
+  className,
+  children,
+  ...props
+}: CredenzaProps) => {
+  const { isDesktop } = useMediaQuery();
+  const CredenzaDescription = isDesktop ? DialogDescription : DrawerDescription;
+
+  return (
+    <CredenzaDescription className={className} {...props}>
+      {children}
+    </CredenzaDescription>
+  );
+};
+
+const CredenzaHeader = ({ className, children, ...props }: CredenzaProps) => {
+  const { isDesktop } = useMediaQuery();
+  const CredenzaHeader = isDesktop ? DialogHeader : DrawerHeader;
+
+  return (
+    <CredenzaHeader className={className} {...props}>
+      {children}
+    </CredenzaHeader>
+  );
+};
+
+const CredenzaTitle = ({ className, children, ...props }: CredenzaProps) => {
+  const { isDesktop } = useMediaQuery();
+  const CredenzaTitle = isDesktop ? DialogTitle : DrawerTitle;
+
+  return (
+    <CredenzaTitle className={className} {...props}>
+      {children}
+    </CredenzaTitle>
+  );
+};
+
+const CredenzaBody = ({ className, children, ...props }: CredenzaProps) => {
+  const { isDesktop } = useMediaQuery();
+  return (
+    <div className={cn(isDesktop ? "px-0" : "px-4", className)} {...props}>
+      {children}
+    </div>
+  );
+};
+
+const CredenzaFooter = ({ className, children, ...props }: CredenzaProps) => {
+  const { isDesktop } = useMediaQuery();
+  const CredenzaFooter = isDesktop ? DialogFooter : DrawerFooter;
+
+  return (
+    <CredenzaFooter className={className} {...props}>
+      {children}
+    </CredenzaFooter>
+  );
+};
+
+export {
+  Credenza,
+  CredenzaTrigger,
+  CredenzaClose,
+  CredenzaContent,
+  CredenzaDescription,
+  CredenzaHeader,
+  CredenzaTitle,
+  CredenzaBody,
+  CredenzaFooter,
+};
