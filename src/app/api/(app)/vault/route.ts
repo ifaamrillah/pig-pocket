504ba509b7e6ee170553df7e36884f507fa78b60
ifaamrillah <amrillah.work@gmail.com>
2024-12-11 23:25:06 +0700

    feat: add get all vault api

--- src/app/api/(app)/vault/route.ts
+++ src/app/api/(app)/vault/route.ts
@@ -1,10 +1,53 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { prisma } from "@/lib/prisma";
-import { checkFields, checkSession } from "@/lib/utils";
+import { checkFields, checkSession, parseQueryParams } from "@/lib/utils";
 import { VaultValidator } from "@/lib/validator";
 
+export async function GET(req: NextRequest) {
+  // Check session
+  const user = await checkSession();
+  if (user instanceof NextResponse) return user;
+
+  // Query params
+  const { pagination, sorting, filters } = parseQueryParams(
+    req.nextUrl.searchParams
+  );
+
+  // Filter
+  const where = {
+    userId: user.id,
+    name: filters?.name || undefined,
+  };
+
+  // Get all vault
+  const [getAllVault, totalData] = await Promise.all([
+    prisma.vault.findMany({
+      where,
+      skip: (pagination.pageIndex - 1) * pagination.pageSize,
+      take: pagination.pageSize,
+      orderBy: sorting.orderBy,
+    }),
+    prisma.vault.count({ where }),
+  ]);
+
+  return NextResponse.json({
+    message: getAllVault.length
+      ? "Get all vault successfully."
+      : "No vaults found.",
+    data: getAllVault,
+    filters,
+    pagination: {
+      ...pagination,
+      totalData,
+      totalPage: Math.ceil(totalData / pagination.pageSize),
+    },
+    sorting,
+  });
+}
+
 export async function POST(req: NextRequest) {
+  // Session
   const user = await checkSession();
   if (user instanceof NextResponse) return user;
 
