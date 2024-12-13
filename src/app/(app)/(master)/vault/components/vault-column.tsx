02b3e86c8b6b29344abd8c7ba078ecbe121db66b
ifaamrillah <amrillah.work@gmail.com>
2024-12-13 13:49:59 +0700

    feat: add edit vault button

--- src/app/(app)/(master)/vault/components/vault-column.tsx
+++ src/app/(app)/(master)/vault/components/vault-column.tsx
@@ -1,10 +1,24 @@
+"use client";
+import { useState } from "react";
+import { Settings, SquarePen } from "lucide-react";
+
 import {
+  ActionColumn,
   CurrencyColumn,
   DefaultColumn,
   NameColumn,
   NoColumn,
 } from "@/components/data-table/table-column";
 import { Badge } from "@/components/ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
+import { VaultModal } from "./vault-modal";
 
 export const vaultColumn = [
   NoColumn({
@@ -36,4 +50,42 @@ export const vaultColumn = [
       </div>
     ),
   }),
+  ActionColumn({
+    accessorKey: "actions",
+    header: "Action",
+    cell: ({ row }) => <ActionButton id={row.getValue("id")} />,
+  }),
 ];
+
+const ActionButton = ({ id }: { id: string }) => {
+  const [isModalEditOpen, setModalEditOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="size-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <Settings className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem
+            onClick={() => setModalEditOpen(true)}
+            className="cursor-pointer"
+          >
+            <SquarePen className="size-4" /> Edit
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {isModalEditOpen && (
+        <VaultModal
+          id={id}
+          isOpen={isModalEditOpen}
+          setOpen={setModalEditOpen}
+        />
+      )}
+    </>
+  );
+};
