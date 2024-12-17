c50ccd8744090f108b6e637235f0811acbfe2d88
ifaamrillah <amrillah.work@gmail.com>
2024-12-17 09:53:04 +0700

    feat: add category type constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,6 @@
 export const FREE_PLAN_DURATION = 15 * 24 * 60 * 60 * 1000; // 15 days
+
+export const CATEGORY_TYPE = [
+  { value: "INCOME", label: "Income" },
+  { value: "EXPENSE", label: "Expense" },
+];
