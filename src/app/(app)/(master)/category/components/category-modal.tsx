73ac30028565eabe3cc0d8e0a98824c285159892
ifaamrillah <amrillah.work@gmail.com>
2024-12-17 09:53:22 +0700

    feat: add create category form

--- src/app/(app)/(master)/category/components/category-modal.tsx
+++ src/app/(app)/(master)/category/components/category-modal.tsx
@@ -1,4 +1,14 @@
 import { Dispatch, SetStateAction } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { toast } from "sonner";
+
+import { CategoryValidator, TypeCategoryValidator } from "@/lib/validator";
+import { CATEGORY_TYPE } from "@/lib/constants";
+
+import { createCategory } from "@/services/category-service";
 
 import {
   Credenza,
@@ -10,6 +20,9 @@ import {
   CredenzaTitle,
 } from "@/components/ui/credenza";
 import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { FormInput } from "@/components/form/form-input";
+import { FormSelect } from "@/components/form/form-select";
 
 interface CategoryModalProps {
   isOpen: boolean;
@@ -17,18 +30,81 @@ interface CategoryModalProps {
 }
 
 export const CategoryModal = ({ isOpen, setOpen }: CategoryModalProps) => {
+  const queryClient = useQueryClient();
+
+  const { mutate: mutateCreateCategory, isPending: isPendingCreateCategory } =
+    useMutation({
+      mutationFn: (values: TypeCategoryValidator) => createCategory(values),
+      onSuccess: () => {
+        toast.success("Create category successfully.");
+      },
+      onError: (err: AxiosError<{ message: string }>) => {
+        toast.error(err?.response?.data?.message || "Create category failed.");
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries({ queryKey: ["getAllCategory"] });
+        setOpen(false);
+      },
+    });
+
+  const form = useForm<TypeCategoryValidator>({
+    resolver: zodResolver(CategoryValidator),
+    values: {
+      name: "",
+      type: {
+        value: "EXPENSE",
+        label: "Expense",
+      },
+    },
+  });
+
+  const onSubmit = (values: TypeCategoryValidator) => {
+    mutateCreateCategory(values);
+  };
+
   return (
     <Credenza open={isOpen} onOpenChange={setOpen}>
       <CredenzaContent>
         <CredenzaHeader>
           <CredenzaTitle>Add New Category</CredenzaTitle>
         </CredenzaHeader>
-        <CredenzaBody>Category Modal</CredenzaBody>
+        <CredenzaBody>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-4 my-4"
+            >
+              <FormInput
+                form={form}
+                name="name"
+                label="Name"
+                placeholder="Category name"
+                required
+                disabled={isPendingCreateCategory}
+              />
+              <FormSelect
+                form={form}
+                name="type"
+                label="Type"
+                options={CATEGORY_TYPE}
+                required
+                disabled={isPendingCreateCategory}
+              />
+            </form>
+          </Form>
+        </CredenzaBody>
         <CredenzaFooter>
           <CredenzaClose asChild>
-            <Button variant="outline">Cancel</Button>
+            <Button variant="outline" disabled={isPendingCreateCategory}>
+              Cancel
+            </Button>
           </CredenzaClose>
-          <Button>Save</Button>
+          <Button
+            onClick={form.handleSubmit(onSubmit)}
+            disabled={isPendingCreateCategory}
+          >
+            {isPendingCreateCategory ? "Saving..." : "Save"}
+          </Button>
         </CredenzaFooter>
       </CredenzaContent>
     </Credenza>
