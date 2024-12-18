7cae16ea04d8bdf146c8299bd6d1e4cda3a34542
ifaamrillah <amrillah.work@gmail.com>
2024-12-18 09:38:45 +0700

    feat: add get all category api

--- src/app/api/(app)/category/route.ts
+++ src/app/api/(app)/category/route.ts
@@ -1,8 +1,53 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { prisma } from "@/lib/prisma";
-import { checkFields, checkSession } from "@/lib/utils";
+import { checkFields, checkSession, parseQueryParams } from "@/lib/utils";
 import { CategoryValidator } from "@/lib/validator";
+
+export async function GET(req: NextRequest) {
+  // Session
+  const user = await checkSession();
+  if (user instanceof NextResponse) return user;
+
+  // Query params
+  const { pagination, sorting, filters } = parseQueryParams(
+    req.nextUrl.searchParams
+  );
+  const filterName = filters?.name || undefined;
+
+  // Filter
+  const where = {
+    userId: user.id,
+    ...(filterName && {
+      name: { contains: filterName, mode: "insensitive" },
+    }),
+  };
+
+  // Get all category
+  const [getAllCategory, totalData] = await Promise.all([
+    prisma.category.findMany({
+      where,
+      skip: (pagination.pageIndex - 1) * pagination.pageSize,
+      take: pagination.pageSize,
+      orderBy: sorting.orderBy,
+    }),
+    prisma.category.count({ where }),
+  ]);
+
+  return NextResponse.json({
+    message: getAllCategory.length
+      ? "Get all category successfully."
+      : "No categories found.",
+    data: getAllCategory,
+    filters,
+    pagination: {
+      ...pagination,
+      totalData,
+      totalPage: Math.ceil(totalData / pagination.pageSize),
+    },
+    sorting,
+  });
+}
 
 export async function POST(req: NextRequest) {
   // Session
