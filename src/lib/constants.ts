282743c9e763f8e31f3b0b56354c8beac7289c39
ifaamrillah <amrillah.work@gmail.com>
2024-12-18 09:38:27 +0700

    feat: add color badge at category type constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const FREE_PLAN_DURATION = 15 * 24 * 60 * 60 * 1000; // 15 days
 
 export const CATEGORY_TYPE = [
-  { value: "INCOME", label: "Income" },
-  { value: "EXPENSE", label: "Expense" },
+  { value: "INCOME", label: "Income", color: "green-500" },
+  { value: "EXPENSE", label: "Expense", color: "red-500" },
 ];
