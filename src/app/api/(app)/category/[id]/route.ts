95e944b1da7fb758f3a358d47fb465c3d7673db0
ifaamrillah <amrillah.work@gmail.com>
2024-12-19 17:39:13 +0700

    feat: add update category by id api

--- src/app/api/(app)/category/[id]/route.ts
+++ src/app/api/(app)/category/[id]/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { prisma } from "@/lib/prisma";
-import { checkSession } from "@/lib/utils";
+import { checkFields, checkSession } from "@/lib/utils";
+import { CategoryValidator } from "@/lib/validator";
 
 export async function GET(
   req: NextRequest,
@@ -33,5 +34,78 @@ export async function GET(
       data: getCategoryById,
     },
     { status: 200 }
+  );
+}
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  // Check session
+  const user = await checkSession();
+  if (user instanceof NextResponse) return user;
+
+  // Params
+  const id = (await params).id;
+
+  // Check id is valid
+  const getCategoryById = await prisma.category.findUnique({
+    where: { id },
+    select: {
+      id: true,
+    },
+  });
+  if (!getCategoryById) {
+    return NextResponse.json(
+      { message: `Category with id: "${id}" was not found.` },
+      { status: 404 }
+    );
+  }
+
+  // Body
+  const body = await checkFields(req, CategoryValidator);
+  if (body instanceof NextResponse) return body;
+
+  // Check name is unique
+  const existingCategoryName = await prisma.category.findUnique({
+    where: {
+      userId_name: {
+        userId: user.id as string,
+        name: body.name,
+      },
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  });
+  if (existingCategoryName && existingCategoryName?.id !== id) {
+    return NextResponse.json(
+      { message: `Category with name: "${body.name}" already exists.` },
+      { status: 409 }
+    );
+  }
+
+  // Update category by id
+  const updateCategoryById = await prisma.category.update({
+    where: {
+      id,
+    },
+    data: body,
+  });
+  if (updateCategoryById) {
+    return NextResponse.json(
+      {
+        message: "Edit category successfully.",
+        data: updateCategoryById,
+      },
+      { status: 200 }
+    );
+  }
+
+  // Internal server error
+  return NextResponse.json(
+    { message: "Edit category failed." },
+    { status: 500 }
   );
 }
