470725d9f5bb505a0a8fa6ca301a24acc7a6929c
ifaamrillah <amrillah.work@gmail.com>
2024-12-19 13:48:18 +0700

    feat: add vault status constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,10 @@
 export const FREE_PLAN_DURATION = 15 * 24 * 60 * 60 * 1000; // 15 days
 
+export const VAULT_STATUS = [
+  { value: true, label: "Active", color: "green-500" },
+  { value: false, label: "Inactive", color: "red-500" },
+];
+
 export const CATEGORY_TYPE = [
   { value: "INCOME", label: "Income", color: "green-500" },
   { value: "EXPENSE", label: "Expense", color: "red-500" },
