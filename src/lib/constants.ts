bc3db417e720e559a7761a3db30eb6631e52420d
ifaamrillah <amrillah.work@gmail.com>
2024-12-20 08:36:51 +0700

    feat: add status type constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,10 @@
 export const FREE_PLAN_DURATION = 15 * 24 * 60 * 60 * 1000; // 15 days
 
+export const STATUS_TYPE = [
+  { value: "ACTIVE", label: "Active", color: "green" },
+  { value: "INACTIVE", label: "Inactive", color: "red" },
+];
+
 export const CATEGORY_TYPE = [
   { value: "INCOME", label: "Income", color: "green" },
   { value: "EXPENSE", label: "Expense", color: "red" },
